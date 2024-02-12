a25312484bb064d7e40074630c35af1a8c716c31
zaneseddon <159535799+zaneseddon@users.noreply.github.com>
2024-02-12 15:35:52 +0000

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main() {
-   printf("Hello. My email address is: \nzane.seddon@student.manchester.ac.uk");
+   printf("Hello. My email address is:\nzane.seddon@student.manchester.ac.uk");
    return 0;
 }
